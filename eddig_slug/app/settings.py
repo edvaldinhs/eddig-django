430c0c8a3786c234cff8b096bca31d43a35629f9
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-03-23 13:56:26 -0300

    settings.py changes for whitenoise

--- eddig_slug/app/settings.py
+++ eddig_slug/app/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'game/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
