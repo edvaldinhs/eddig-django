6f80fe7833d38cd5a041ceb11c7546c22fa6def3
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-03-23 13:48:45 -0300

    ty minor settings changes

--- eddig_slug/app/settings.py
+++ eddig_slug/app/settings.py
@@ -83,7 +83,7 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': tmpPostgres.path.decode().replace('/', ''),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
