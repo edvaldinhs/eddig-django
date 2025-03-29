84563a69004e67c94e253e58caadfff96539257d
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-03-29 01:58:56 -0300

    better tutorial

--- eddig_slug/app/wsgi.py
+++ eddig_slug/app/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
+from whitenoise import WhiteNoise # type: ignore
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
