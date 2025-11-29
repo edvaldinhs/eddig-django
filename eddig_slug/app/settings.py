67429e3491eb785f91739de72e93e96dc3cda0ef
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-11-29 09:30:31 -0300

    update: better responsiveness

--- eddig_slug/app/settings.py
+++ eddig_slug/app/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path, os
+import os
+from pathlib import Path
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 
